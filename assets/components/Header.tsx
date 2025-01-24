a5feded69f0e7492f24ffa4a707d874c4496bcbb
qguillaume <quesnel.guillaume.j@gmail.com>
2025-01-24 19:09:13 +0100

    GUIL-9 header en cours ne prend pas en compte si connecté ou pas

--- assets/components/Header.tsx
+++ assets/components/Header.tsx
@@ -1,15 +1,14 @@
-// src/components/Header.tsx
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useUser } from '../context/UserContext'; // Hook
 import '../styles/components/_header.scss';
 
 const Header: React.FC = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
   const [menuOpen, setMenuOpen] = useState(false);
-  const { user } = useUser(); 
+  const { user } = useUser();
 
-  const isLoggedIn = user !== null;
+  const isLoggedIn = !!user;
   const isAdmin = isLoggedIn && user?.roles.includes('ROLE_ADMIN');
   const isSuperAdmin = isLoggedIn && user?.roles.includes('ROLE_SUPER_ADMIN');
 
@@ -18,6 +17,9 @@ const Header: React.FC = () => {
     setMenuOpen(!menuOpen);
   };
 
+  alert("isLoggedIn : "+isLoggedIn);
+  alert("isAdmin : "+isAdmin);
+  alert("isSuperAdmin : "+isSuperAdmin);
   return (
     <header className="header">
       <div className="hamburger-menu" onClick={toggleMenu}>
@@ -26,15 +28,17 @@ const Header: React.FC = () => {
 
       {/* Menu de navigation affiché lorsque le menu burger est ouvert */}
       <nav className={`nav-menu ${menuOpen ? 'active' : ''}`}>
-        <div className="header-left">
-          <a href="/" className={isLoggedIn ? 'active' : ''}>
-            {t('home')}
+        <div className="header-center">
+          <a href="/" className="logo">
+            <img src="images/logoGQ.png" alt="logo GQ" />
           </a>
+        </div>
+
+        <div className="header-left">
+          <a href="/">{t('home')}</a>
 
           <div className="dropdown">
-            <a href="/photos" className={isLoggedIn ? 'active' : ''}>
-              {t('photos')}
-            </a>
+            <a href="/photos">{t('photos')}</a>
             {isLoggedIn && (
               <div className="dropdown-content">
                 <a href="/album/new">{t('create_album')}</a>
@@ -45,9 +49,7 @@ const Header: React.FC = () => {
 
           {isAdmin && (
             <div className="dropdown">
-              <a href="/articles" className={isLoggedIn ? 'active' : ''}>
-                {t('articles')}
-              </a>
+              <a href="/articles">{t('articles')}</a>
               <div className="dropdown-content">
                 <a href="/articles/new">{t('new_article')}</a>
                 <a href="/theme/create">{t('new_theme')}</a>
@@ -56,41 +58,25 @@ const Header: React.FC = () => {
           )}
 
           {isSuperAdmin && (
-            <a href="/references" className={isLoggedIn ? 'active' : ''}>
-              {t('references')}
-            </a>
+            <a href="/references">{t('references')}</a>
           )}
 
-          <a href="/contact" className={isLoggedIn ? 'active' : ''}>
-            {t('contact')}
-          </a>
-        </div>
-
-        <div className="header-center">
-          <a className="logo" href="/home">
-            <img src="images/logoGQ.png" alt="logo GQ" />
-          </a>
+          <a href="/contact">{t('contact')}</a>
         </div>
 
         <div className="header-right">
           {isLoggedIn ? (
             <div className="dropdown">
-              <a href="#" className={isLoggedIn ? 'active' : ''}>
-                {t('welcome')} {user?.username}!
-              </a>
+              <a href="#">{t('welcome')} {user.username}</a>
               <div className="dropdown-content">
-                {isAdmin && <a href="/dashboard">{t('dashboard')}</a>}
+                {(isAdmin || isSuperAdmin) && <a href="/dashboard">{t('dashboard')}</a>}
                 <a href="/logout">{t('disconnection')}</a>
               </div>
             </div>
           ) : (
             <>
-              <a href="/login" className={isLoggedIn ? 'active' : ''}>
-                {t('connexion')}
-              </a>
-              <a href="/register" className={isLoggedIn ? 'active' : ''}>
-                {t('inscription')}
-              </a>
+              <a href="/login">{t('connexion')}</a>
+              <a href="/register" className="register">{t('inscription')}</a>
             </>
           )}
         </div>
