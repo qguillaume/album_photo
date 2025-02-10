8cc5fcc0a0cfdc6feb939014b4c322061c30c080
qguillaume <quesnel.guillaume.j@gmail.com>
2025-02-10 17:30:26 +0100

    GUIL-62 mode mobile header svg à finaliser

--- assets/components/Header.tsx
+++ assets/components/Header.tsx
@@ -35,7 +35,10 @@ const Header: React.FC = () => {
           {isLoggedIn ? (
             <div className="dropdown">
               <a href="#" className={window.location.pathname === '/photos' ? 'active' : ''}>
-                {t('photos')}
+                <span className="desktop-only">{t('photos')}</span>
+                <svg className="mobile-only" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                  <path d="M21 7h-4.2l-1.7-2.4c-.3-.4-.8-.6-1.3-.6H10c-.5 0-1 .2-1.3.6L7 7H3c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2zm-9 12c-2.8 0-5-2.2-5-5s2.2-5 5-5 5 2.2 5 5-2.2 5-5 5zm0-8c-1.7 0-3 1.3-3 3s1.3 3 3 3 3-1.3 3-3-1.3-3-3-3z"/>
+                </svg>
               </a>
               <div className="dropdown-content">
                 <a href="/photos">{t('see_photos')}</a>
@@ -45,14 +48,20 @@ const Header: React.FC = () => {
             </div>
           ) : (
             <a href="/photos" className={window.location.pathname === '/photos' ? 'active' : ''}>
-              {t('photos')}
+              <span className="desktop-only">{t('photos')}</span>
+              <svg className="mobile-only" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                <path d="M21 7h-4.2l-1.7-2.4c-.3-.4-.8-.6-1.3-.6H10c-.5 0-1 .2-1.3.6L7 7H3c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2zm-9 12c-2.8 0-5-2.2-5-5s2.2-5 5-5 5 2.2 5 5-2.2 5-5 5zm0-8c-1.7 0-3 1.3-3 3s1.3 3 3 3 3-1.3 3-3-1.3-3-3-3z"/>
+              </svg>
             </a>
           )}
 
           {isAdmin && (
             <div className="dropdown">
               <a href="#" className={window.location.pathname === '/articles' ? 'active' : ''}>
-                {t('articles')}
+                <span className="desktop-only">{t('articles')}</span>
+                <svg className="mobile-only" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                  <path d="M3 3h18v18H3V3zm2 2v14h14V5H5zm4 2h6v2H9V7zm0 4h6v2H9v-2zm0 4h6v2H9v-2z"/>
+                </svg>
               </a>
               <div className="dropdown-content">
                 <a href="/articles">{t('see_articles')}</a>
@@ -69,7 +78,10 @@ const Header: React.FC = () => {
           )*/}
 
           <a href="/contact" className={window.location.pathname === '/contact' ? 'active' : ''}>
-            {t('contact')}
+            <span className="desktop-only">{t('contact')}</span>
+            <svg className="mobile-only" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+              <path d="M22 6H2v12h20V6zM20 16H4V8h16v8zm-3-4h-2v-2h2v2zm-4 0h-2v-2h2v2zm-4 0H7v-2h2v2z"/>
+            </svg>
           </a>
         </div>
 
